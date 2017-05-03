fe833fd90ed57bbe9c1cb9b3f3cc36b92981b08d
abhishekhandacse <abhishekhandacse@gmail.com>
2017-05-03 10:08:11 +0530

    Made basic dropbox for uploading photos

---
+++ Register.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abhishek
+ * Date: 27-04-17
+ * Time: 12:23 PM
+ */
---
+++ Upload.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abhishek
+ * Date: 27-04-17
+ * Time: 11:01 PM
+ */
